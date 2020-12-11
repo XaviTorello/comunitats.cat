26cd030df011011a3ba9bab430e0c39ac1a72e0b
Xavi Torelló <info@xaviertorello.cat>
2020-12-11 10:12:48 +0100

    Debug status

--- tools/rebuild_onchanges.sh
+++ tools/rebuild_onchanges.sh
@@ -2,6 +2,8 @@
 # Detect changes to trigger a new rebuild
 
 # something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
+echo `git status --porcelain | wc -l`
+git status
 something_changed=`git status --porcelain | wc -l`
 if [ "$something_changed" -gt 0 ]; then
     echo >&2 "Something changed in $local_ref, rebuilding app"

d81373f8683898fdfbe3dec7b6f133c7e955cb1b
Xavi Torelló <info@xaviertorello.cat>
2020-12-11 10:16:21 +0100

    Troubleshoot CI rebuild process (#3)
    * Detect changes via status porcelain and wc
    
    * Debug status

--- tools/rebuild_onchanges.sh
+++ tools/rebuild_onchanges.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Detect changes to trigger a new rebuild
 
-something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
-if [ -n "$something_changed" ]
-then
+# something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
+echo `git status --porcelain | wc -l`
+git status
+something_changed=`git status --porcelain | wc -l`
+if [ "$something_changed" -gt 0 ]; then
     echo >&2 "Something changed in $local_ref, rebuilding app"
     yarn && yarn build
 fi

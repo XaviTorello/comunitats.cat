0b4177c2880945c3ed1e2edad540a6e62e1f64e6
Xavi Torelló <info@xaviertorello.cat>
2020-12-11 10:07:54 +0100

    Detect changes via status porcelain and wc

--- tools/rebuild_onchanges.sh
+++ tools/rebuild_onchanges.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Detect changes to trigger a new rebuild
 
-something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
-if [ -n "$something_changed" ]
-then
+# something_changed=`git diff-index --exit-code --ignore-submodules HEAD`
+something_changed=`git status --porcelain | wc -l`
+if [ "$something_changed" -gt 0 ]; then
     echo >&2 "Something changed in $local_ref, rebuilding app"
     yarn && yarn build
 fi

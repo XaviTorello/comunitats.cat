872e30c4f13f45460f919d14991674e4719d9d57
Xavi Torelló <info@xaviertorello.cat>
2021-04-09 11:00:54 +0200

    Clean'up

--- src/index.js
+++ src/index.js
@@ -126,18 +126,6 @@ async function* prepareEvents() {
       newSchedule.borderColor = borderColor || bgColor;
       newSchedule.category = 'time';
 
-      // newSchedule.raw.memo = chance.sentence();
-      // newSchedule.raw.creator.name = chance.name();
-      // newSchedule.raw.creator.avatar = chance.avatar();
-      // newSchedule.raw.creator.company = chance.company();
-      // newSchedule.raw.creator.email = chance.email();
-      // newSchedule.raw.creator.phone = chance.phone();
-
-      // if (chance.bool({ likelihood: 20 })) {
-      //   var travelTime = chance.minute();
-      //   newSchedule.goingDuration = travelTime;
-      //   newSchedule.comingDuration = travelTime;
-      // }
       scheduleList.push(newSchedule);
     },
   );
@@ -176,63 +164,6 @@ async function* prepareEvents() {
       narrowWeekend: true,
     },
   });
-  window.calendar = calendar;
-
-  // // event handlers
-  // calendar.on({
-  //   clickMore: function (e) {
-  //     console.log('clickMore', e);
-  //   },
-  //   clickSchedule: function (e) {
-  //     console.log('clickSchedule', e);
-  //   },
-  //   clickDayname: function (date) {
-  //     console.log('clickDayname', date);
-  //   },
-  //   beforeCreateSchedule: function (e) {
-  //     console.log('beforeCreateSchedule', e);
-  //     saveNewSchedule(e);
-  //   },
-  //   beforeUpdateSchedule: function (e) {
-  //     var schedule = e.schedule;
-  //     var changes = e.changes;
-
-  //     console.log('beforeUpdateSchedule', e);
-
-  //     if (changes && !changes.isAllDay && schedule.category === 'allday') {
-  //       changes.category = 'time';
-  //     }
-
-  //     calendar.updateSchedule(schedule.id, schedule.calendarId, changes);
-  //     refreshScheduleVisibility();
-  //   },
-  //   beforeDeleteSchedule: function (e) {
-  //     console.log('beforeDeleteSchedule', e);
-  //     calendar.deleteSchedule(e.schedule.id, e.schedule.calendarId);
-  //   },
-  //   afterRenderSchedule: function (e) {
-  //     var schedule = e.schedule;
-  //     // var element = calendar.getElement(schedule.id, schedule.calendarId);
-  //     // console.log('afterRenderSchedule', element);
-  //   },
-  //   clickTimezonesCollapseBtn: function (timezonesCollapsed) {
-  //     console.log('timezonesCollapsed', timezonesCollapsed);
-
-  //     if (timezonesCollapsed) {
-  //       calendar.setTheme({
-  //         'week.daygridLeft.width': '77px',
-  //         'week.timegridLeft.width': '77px',
-  //       });
-  //     } else {
-  //       calendar.setTheme({
-  //         'week.daygridLeft.width': '60px',
-  //         'week.timegridLeft.width': '60px',
-  //       });
-  //     }
-
-  //     return true;
-  //   },
-  // });
 
   const resizeThrottled = tui.util.throttle(() => {
     calendar.render();
